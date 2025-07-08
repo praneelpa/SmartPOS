fca412388619db444b568b76fa51273c1ea4b521
praneelpa <153566595+praneelpa@users.noreply.github.com>
2025-07-08 08:55:59 +0000

    main

--- src/main/java/com/smartpos/Main.java
+++ src/main/java/com/smartpos/Main.java
@@ -1,5 +1,17 @@
 package com.smartpos;
 
-public class Main {
-    
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("SmartPOS Test");
+        primaryStage.show();
+    }
 }
