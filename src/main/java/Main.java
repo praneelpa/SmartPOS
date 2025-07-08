999fd6db4f207b2c3e6ab7506f6b3d2dc48f01fc
praneelpa <153566595+praneelpa@users.noreply.github.com>
2025-07-08 18:57:04 +0000

    m

---
+++ src/main/java/Main.java
@@ -0,0 +1,20 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        Label label = new Label("Hello, World!");
+        Scene scene = new Scene(label, 300, 200);
+
+        primaryStage.setTitle("JavaFX Hello World");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
